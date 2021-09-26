a9a68ef92746a090e79d8807bb9de30079d3e531
yandanyang <yandy_java@163.com>
2021-09-26 17:09:01 +0800

    删除无用import

--- admin-api/java-api/src/main/java/net/lab1024/smartadmin/service/config/AdminWebAppConfig.java
+++ admin-api/java-api/src/main/java/net/lab1024/smartadmin/service/config/AdminWebAppConfig.java
@@ -1,13 +1,8 @@
 package net.lab1024.smartadmin.service.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Map;
 
 /**
  * @Description
