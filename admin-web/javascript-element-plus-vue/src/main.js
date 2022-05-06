6f47bdf01bf448b8a8b5d4b49611d0835ae0f0c7
kaiyun <1183962242@qq.com>
2022-05-06 18:56:01 +0800

    修改登录页面

--- admin-web/javascript-element-plus-vue/src/main.js
+++ admin-web/javascript-element-plus-vue/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from '/@/router/index';
 import { store } from "/@/store/index";
-
+import './theme/index.scss';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css'
 import * as icons from "@element-plus/icons";
--- admin-web/javascript-element-plus-vue/src/views/system/login/login.vue
+++ admin-web/javascript-element-plus-vue/src/views/system/login/login.vue
@@ -1,69 +1,92 @@
 <template>
   <div class="login-container">
-    <div class="logo-box">
-      <img class="logo" src="/images/logo.png" />
-    </div>
-    <div class="login-box">
-      <div class="box-item">
-        <div class="welcome">
-          <p>欢迎登录SmartAdmin</p>
-          <p class="desc">保持谦逊，不断学习，热爱代码，热爱生活 !</p>
+    <div class="box-item desc">
+      <div class="welcome">
+        <p>欢迎登录 SmartAdmin V2</p>
+        <p class="desc">
+          SmartAdmin 是由 河南 · 洛阳 <strong>1024创新实验室（1024Lab）</strong>
+          使用SpringBoot2.x 和 Vue3.2 Setup标签、 Composition Api
+          (同时支持JavaScript和TypeScript双版本)
+          ，开发出的一套简洁、易用的中后台解决方案！
+          <br />
+          <br />
+          <span class="setence">
+            致伟大的开发者 ：
+            <br />
+            我们希望用一套漂亮优雅的代码和一套整洁高效的代码规范，让大家在这浮躁的世界里感受到一股把代码写好的清流
+            !
+            <br />
+            保持谦逊，不断学习，热爱代码，更热爱生活 !
+            <span class="author">1024创新实验室 ( 2022年 · 洛阳 ）</span>
+          </span>
+        </p>
+      </div>
+      <div class="app-qr-box">
+        <div class="app-qr">
+          <img src="/images/1024lab-gzh.jpg" />
+          <span class="qr-desc"> 关注公众号 </span>
         </div>
-        <div class="login-person">
-          <div class="img-box">
-            <img src="/images/login-person.png" />
-          </div>
-          <span class="qr-desc">SmartAdmin2.0 由1024创新实验室强力驱动</span>
+        <div class="app-qr">
+          <img src="/images/1024lab-gzh.jpg" />
+          <span class="qr-desc"> 扫码 骚扰卓大 :) </span>
         </div>
       </div>
-      <div class="box-item login">
-        <img class="login-qr" src="/images/login-qr.png" />
-        <div class="login-title">账号登录</div>
-        <el-form ref="formRef" class="login-form" :model="loginForm" :rules="rules">
-          <el-form-item name="loginName">
-            <el-input v-model="loginForm.loginName" placeholder="请输入用户名" />
-          </el-form-item>
-          <el-form-item name="loginPwd">
-            <el-input
-                v-model="loginForm.loginPwd"
-                :type="showPwd ? 'text' : 'password'"
-                placeholder="请输入密码"
+    </div>
+    <div class="box-item login">
+      <img class="login-qr" src="/images/login-qr.png" />
+      <div class="login-title">账号登录</div>
+      <el-form ref="formRef" class="login-form" :model="loginForm" :rules="rules">
+        <el-form-item name="loginName">
+          <el-input v-model="loginForm.loginName" placeholder="请输入用户名" />
+        </el-form-item>
+        <el-form-item name="password">
+          <el-input
+              v-model="loginForm.password"
+              :type="showPassword ? 'text' : 'password'"
+              placeholder="请输入密码"
+          />
+          <div class="eye-box">
+            <img
+                class="eye-icon"
+                v-show="!showPassword"
+                @click="onShowPassword"
+                src="/images/login-form-open-eyes-close.png"
             />
-            <div class="eye-box">
-              <img
-                  class="eye-icon"
-                  v-show="!showPwd"
-                  @click="onShowPwd"
-                  src="/images/login-form-open-eyes-close.png"
-              />
-              <img
-                  class="eye-icon"
-                  v-show="showPwd"
-                  @click="onShowPwd"
-                  src="/images/login-form-open-eyes.png"
-              />
-            </div>
-          </el-form-item>
-          <el-form-item>
-            <el-checkbox v-model="rememberPwd">记住密码</el-checkbox>
-          </el-form-item>
-          <el-form-item>
-            <div class="btn" @click="handleFinish">登录</div>
-          </el-form-item>
-        </el-form>
-        <div class="more">
-          <div class="title-box">
-            <p class="line"></p>
-            <p class="title">其他方式登录</p>
-            <p class="line"></p>
+            <img
+                class="eye-icon"
+                v-show="showPassword"
+                @click="onShowPassword"
+                src="/images/login-form-open-eyes.png"
+            />
           </div>
-          <div class="login-type">
-            <img src="/images/wechat-icon.png" />
-            <img src="/images/qq-icon.png" />
-            <img src="/images/weibo-icon.png" />
-            <img src="/images/google-icon.png" />
-            <img src="/images/ali-icon.png" />
-          </div>
+        </el-form-item>
+        <el-form-item name="loginName">
+          <el-input
+              class="captcha-input"
+              v-model="loginForm.captchaCode"
+              placeholder="请输入验证码"
+          />
+          <img class="captcha-img" :src="captchaBase64Image" @click="getCaptcha" />
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="rememberPwd">记住密码</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <div class="btn" @click="handleFinish">登录</div>
+        </el-form-item>
+      </el-form>
+      <div class="more">
+        <div class="title-box">
+          <p class="line"></p>
+          <p class="title">其他方式登录</p>
+          <p class="line"></p>
+        </div>
+        <div class="login-type">
+          <img src="/images/wechat-icon.png" />
+          <img src="/images/qq-icon.png" />
+          <img src="/images/weibo-icon.png" />
+          <img src="/images/google-icon.png" />
+          <img src="/images/ali-icon.png" />
         </div>
       </div>
     </div>
