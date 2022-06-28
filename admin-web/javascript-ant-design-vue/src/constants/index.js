a8ae27f52ff5464e3d896a7454168acac48f44b7
zhuoda <zhuoluodada@qq.com>
2022-06-28 15:21:54 +0800

    添加登录日志

--- admin-web/javascript-ant-design-vue/src/constants/index.js
+++ admin-web/javascript-ant-design-vue/src/constants/index.js
@@ -4,25 +4,27 @@
  * @Author: zhuoda
  * @Date: 2021-08-11 18:14:13
  * @LastEditors: zhuoda
- * @LastEditTime: 2022-06-23
+ * @LastEditTime: 2022-06-28
  */
-import menu from './system/menu-const'
-import goods from './business/goods-const'
-import category from './business/category-const'
-import { LOGIN_DEVICE_ENUM } from './system/login-device-const'
-import { FLAG_NUMBER_ENUM, GENDER_ENUM } from './common-const'
-import { LAYOUT_ENUM } from './layout-const'
-import file from './business/file-const'
-import notice from './business/notice-const'
+import menu from './system/menu-const';
+import goods from './business/goods-const';
+import category from './business/category-const';
+import { LOGIN_DEVICE_ENUM } from './system/login-device-const';
+import { FLAG_NUMBER_ENUM, GENDER_ENUM } from './common-const';
+import { LAYOUT_ENUM } from './layout-const';
+import file from './business/file-const';
+import notice from './business/notice-const';
+import loginLog from './support/login-log-const';
 
 export default {
   FLAG_NUMBER_ENUM,
   LOGIN_DEVICE_ENUM,
   GENDER_ENUM,
   LAYOUT_ENUM,
+  ...loginLog,
   ...menu,
   ...goods,
   ...category,
   ...file,
-  ...notice
-}
+  ...notice,
+};
